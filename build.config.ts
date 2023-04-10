e1bb34cc68bad79a0118753d1d771d5d554f1aa8
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-04-10 14:22:51 +0800

    Fixed inlined deps

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,11 @@
 import { defineBuildConfig } from "unbuild";
+import { devDependencies } from "./package.json";
 
 export default defineBuildConfig({
     declaration: true,
     entries: [{ input: "./src/index", format: "esm", declaration: true }],
     rollup: {
+        inlineDependencies: true,
         esbuild: {
             minify: true,
         },
