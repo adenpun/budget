6d4a72677020fd42c20152dc1684cc14db0428f2
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-04-12 16:54:58 +0800

    exported some more utils

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import {
     MonthCompare,
 } from "./utils";
 export type * from "./budget-version-1.d.ts";
-export { GetClosestLastMonth, GetLatestMonth, SortMonth } from "./utils";
+export { DateToMonth, GetClosestLastMonth, GetLatestMonth, MonthCompare, SortMonth } from "./utils";
 
 export class Budget {
     private m_budget: BudgetType1 = {
